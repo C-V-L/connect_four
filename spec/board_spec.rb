79ee803662b6134f88ea321d9b3a9ea8858cabbe
Conner Van Loan <cdvanloan@gmail.com>
2022-12-05 18:35:55 -0700

    Creates game_board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Board do
         board = Board.new
         expect(board).to be_a(Board)
     end
-    
+
+    it 'has a game board' do
+        board = Board.new
+        expect(board.game_board.count).to eq(6)
+    end
 end
