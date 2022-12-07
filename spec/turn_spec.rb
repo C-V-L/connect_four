927bff3c5c6c6def40b31885413eef2ce919824b
KaraJH <karas.jones@yahoo.com>
2022-12-06 17:12:30 -0700

    test: Computer selection method and fixing spacing

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -12,28 +12,44 @@ RSpec.describe Turn do
         expect(turn.player1).to eq(player1)
         expect(turn.board).to eq(board)
     end
-    it 'can check if selected row is available' do
-        board = Board.new
-        player1 = Player.new("Benedict", "X")
-        turn = Turn.new(player1, board)
-        expect(turn.open_column?('A')).to eq(true)
-        turn.board.game_board[0][0] = "X"
-        expect(turn.open_column?('A')).to eq(false)
+
+    describe '#open_column?' do
+        it 'can check if selected row is available' do
+            board = Board.new
+            player1 = Player.new("Benedict", "X")
+            turn = Turn.new(player1, board)
+            expect(turn.open_column?('A')).to eq(true)
+            turn.board.game_board[0][0] = "X"
+            expect(turn.open_column?('A')).to eq(false)
+        end
     end
 
-    it 'can place a peice in lowest available row' do
-        board = Board.new
-        player1 = Player.new("Benedict", "X")
-        turn = Turn.new(player1, board)
+   describe '#place_piece' do 
+        it 'can place a peice in lowest available row' do
+            board = Board.new
+            player1 = Player.new("Benedict", "X")
+            turn = Turn.new(player1, board)
 
-        turn.place_piece('A')
+            turn.place_piece('A')
+            
+            expect(turn.board.game_board[5][0]).to eq("X")
+            turn.place_piece('A')
+            expect(turn.board.game_board[4][0]).to eq("X")
+            turn.place_piece('C')
+            expect(turn.board.game_board[5][2]).to eq("X")
         
-        expect(turn.board.game_board[5][0]).to eq("X")
-        turn.place_piece('A')
-        expect(turn.board.game_board[4][0]).to eq("X")
-        turn.place_piece('C')
-        expect(turn.board.game_board[5][2]).to eq("X")
-        #require 'pry'; binding.pry
+        end
+    end
 
+    describe'#computer_selection' do 
+        it 'select a random valid placement for the board' do 
+            board = Board.new
+            player1 = Player.new("Benedict", "X")
+            turn = Turn.new(player1, board)
+
+            expect(turn.computer_player).to be_a(Player)
+            #expect(turn.computer_selection).to include("A")
+
+        end
     end
 end
