edb3a53af27bfefa3130aad417fd6cb0a42a33ed
KaraJH <karas.jones@yahoo.com>
2022-12-08 16:38:23 -0700

    feat: Adds play connect four method in game class

--- lib/game.rb
+++ lib/game.rb
@@ -3,14 +3,14 @@ class Game
     attr_reader :turn, :player, :board
     def initialize
         @board = Board.new
-        @player = Player.new("", "X")
+        @player1 = Player.new("", "X")
         @turn = Turn.new(@player1, @board)
     end
 
     def create_player
         puts "Hello, player! Please enter your name below"
         user_name_entry = gets.chomp.capitalize
-        @player = Player.new(user_name_entry, "X")
+        @player1 = Player.new(user_name_entry, "X")
     end
 
     def welcome_message
@@ -38,6 +38,45 @@ class Game
         #{board.game_board[4].join(" ")}
         #{board.game_board[5].join(" ")}
         "
+    end
+
+    def play_connect_four
+    
+        until (turn.check_win_diagonal_downward != false) || (turn.check_win_diagonal_upward != false) ||
+              (turn.check_win_horizontal != false) || (turn.check_win_vertical != false) || (check_for_draw == true)
+
+            puts "Choose a column from A - G to place your piece" 
+            user_column_choice = gets.chomp.upcase
+
+            if turn.open_column?(user_column_choice) == true 
+              
+                turn.place_piece(user_column_choice)
+                computer_turn = turn.computer_selection
+                turn.computer_place_piece(computer_turn)
+                print_board
+              
+                turn.check_win_diagonal_downward
+                turn.check_win_diagonal_upward
+                turn.check_win_horizontal
+                turn.check_win_vertical
+                
+            else  
+                
+                puts "#{user_column_choice}: This is not an available column"
+                self.play_connect_four
+            end
+        end
+
+        puts   "#{turn.check_all_wins.name}"
+    
+    end
+
+    def check_for_draw
+        if @board.game_board[0].include?(".") == false 
+            true 
+        else
+            false
+        end
     end
 
 end
